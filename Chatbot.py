7383e515a8ebb4bc32fa951129c363bffb9a9c31
Mokshada2721 <sablemokshada@gmail.com>
2024-05-04 12:44:35 +0530

    revised

--- Chatbot.py
+++ Chatbot.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+from langdetect import detect
 
 API_URL = "https://api-inference.huggingface.co/models/l3cube-pune/marathi-gpt"
 HEADERS = {"Authorization": "Bearer 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"}
@@ -8,8 +9,31 @@ def query(payload):
     response = requests.post(API_URL, headers=HEADERS, json=payload)
     return response.json()
 
+def remove_english_words(text):
+    # Detect the language of the text
+    language = detect(text)
+    # If the detected language is English, remove English words
+    if language == "en":
+        # Define English alphabet characters
+        english_alphabet = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        # Filter out English words
+        filtered_text = "".join([char for char in text if char not in english_alphabet])
+        return filtered_text
+    else:
+        return text
+
+
+def generate_response(prompt):
+    max_input_length = 50  # Maximum allowed input length
+    prompt = remove_english_words(prompt)
+    prompt = prompt[:max_input_length]  # Take only the first max_input_length characters
+    #prompt += " summarize"  # Add "summarize" to the prompt if summarization is requested
+    output = query({"inputs": prompt})
+    return output[0]['generated_text'] if output and isinstance(output, list) and len(output) > 0 else None
+
+
 st.title("💬 Vachanakar")
-st.caption("🚀 A chatbot to summarize text in Marathi")
+st.caption("🚀 A streamlit chatbot powered by Hugging Face's Marathi GPT")
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
@@ -20,11 +44,9 @@ for msg in st.session_state.messages:
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    output = query({"inputs": prompt, "max_length": 200, "max_new_tokens": 50})  # Adjust max_length and max_new_tokens
-    st.write("Response from Hugging Face API:", output)  # Print the output for debugging
-    if output and 'generated_text' in output[0]:
-        msg = output[0]['generated_text']
-        st.session_state.messages.append({"role": "assistant", "content": msg})
-        st.chat_message("assistant").write(msg)
+    response = generate_response(prompt)
+    if response:
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.chat_message("assistant").write(response)
     else:
         st.error("Error: Unable to generate response")
